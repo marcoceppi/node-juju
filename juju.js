e766096337a01888b701914cfecb757969fe91a7
Marco Ceppi <marco@ceppi.net>
2012-08-10 11:38:02 -0400

    Collapsed Juju object to different methods

--- juju.js
+++ juju.js
@@ -1,90 +1,89 @@
-var juju =
+var exec = 	function(container, opts, cb)
 {
-	exec: function(container, opts, cb)
+	config = { env: process.env, timeout: 240000 };
+	config.env.HOME = container;
+	console.log('juju '+command+' '+arguments(opts));
+	exec('juju '+command+' '+arguments(opts), config, function(err_arr, results, err_str)
 	{
-		config = { env: process.env, timeout: 240000 };
-		config.env.HOME = container;
-		console.log('juju '+command+' '+this.parse_arguments(opts));
-		exec('juju '+command+' '+this.parse_arguments(opts), config, function(err_arr, results, err_str)
-		{
-			cb(err_arr, results);
-		});
-	},
-	status: function(container, job, cb)
-	{
-		opts = {e: job.environment, format: "json"};
-
-		juju.exec(container, command, opts, function(error, results)
-		{
-			results = results || JSON.stringify({});
-			results = JSON.parse(results);
-
-			// Build a status object
-			if( error )
-			{
-				data = { bootstrapped: false, units: 0, services: 0, data: JSON.stringify(results) };
-			}
-			else
-			{
-				data =
-				{
-					services: Object.keys(results.services).length,
-					units: Object.keys(results.machines).length,
-					bootstrapped: Object.keys(results.machines).length > 0,
-					data: JSON.stringify(results)
-				};
-			}
-
-			// Do we even need a callback?
-			redisClient.HMSET(job.user+':'+job.environment+':'+job.action, data, function(err, res){});
-
-			opts.format = 'png';
-			juju.exec(container, command, opts, function(error, results)
-			{
-				if( !error )
-				{
-					redisClient.SET(job.user+':'+job.environment+':layout', results);
-					cb(null, container, job);
-				}
-				else
-				{
-					cb(error, container, job);
-				}
-			});
-		});
-	},
-	bootstrap: function(container, job, cb)
-	{
-		opts = {e: job.environment};
-
-		juju.exec(container, command, opts, function(error, results)
-		{
-			cb(error, container, job);
-		});
-	},
-	arguments: function(args)
-	{
-		args_s = "";
-		for(var key in args)
-		{
-			if( key == 'argv' )
-			{
-				argv = "";
-				for(var el in args[key])
-				{
-					argv += ((argv) ? " " : "")+args[key];
-				}
-
-				args_s += ((args_s) ? " " : "")+argv;
-			}
-			else
-			{
-				args_s += ((args_s) ? " " : "")+((typeof key == "number") ? "" : ((key.length == 1) ? "-" : "--"))+key+"="+args[key];
-			}
-		}
-
-		return args_s;
-	}
+		cb(err_arr, results);
+	});
 }
 
-exports.juju = juju;
+var arguments = function(args)
+{
+	args_s = "";
+	for(var key in args)
+	{
+		if( key == 'argv' )
+		{
+			argv = "";
+			for(var el in args[key])
+			{
+				argv += ((argv) ? " " : "")+args[key];
+			}
+
+			args_s += ((args_s) ? " " : "")+argv;
+		}
+		else
+		{
+			args_s += ((args_s) ? " " : "")+((typeof key == "number") ? "" : ((key.length == 1) ? "-" : "--"))+key+"="+args[key];
+		}
+	}
+
+	return args_s;
+}
+
+exports.status = function(container, job, cb)
+{
+	opts = {e: job.environment, format: "json"};
+
+	exec(container, command, opts, function(error, results)
+	{
+		results = results || JSON.stringify({});
+		results = JSON.parse(results);
+
+		// Build a status object
+		if( error )
+		{
+			data = { bootstrapped: false, units: 0, services: 0, data: JSON.stringify(results) };
+		}
+		else
+		{
+			data =
+			{
+				services: Object.keys(results.services).length,
+				units: Object.keys(results.machines).length,
+				bootstrapped: Object.keys(results.machines).length > 0,
+				data: JSON.stringify(results)
+			};
+		}
+
+		// Do we even need a callback?
+		redisClient.HMSET(job.user+':'+job.environment+':'+job.action, data, function(err, res){});
+
+		opts.format = 'png';
+		juju.exec(container, command, opts, function(error, results)
+		{
+			if( !error )
+			{
+				redisClient.SET(job.user+':'+job.environment+':layout', results);
+				cb(null, container, job);
+			}
+			else
+			{
+				cb(error, container, job);
+			}
+		});
+	});
+}
+
+exports.bootstrap = function(container, job, cb)
+{
+	opts = {e: job.environment};
+
+	exec(container, command, opts, function(error, results)
+	{
+		cb(error, container, job);
+	});
+}
+
