c14417635aee01c79e83796f780e09ef9df9e028
Marco Ceppi <marco@ceppi.net>
2012-10-01 16:53:00 -0400

    Finished Bootstrap tests

--- tests/subs/bootstrap.js
+++ tests/subs/bootstrap.js
@@ -2,12 +2,14 @@ var Juju = require('../../index.js', true);
 
 exports.setUp = function(cb)
 {
-	
+	// Create a dummy environment
+	juju = new Juju('stupid');
 	cb();
 }
 
 exports.tearDown = function(cb)
 {
+	juju = null;
 	cb();
 }
 
@@ -15,20 +17,27 @@ exports.testJujuExists = function(test)
 {
 	test.ok(true, "Juju exists, we can continue");
 	test.done();
-};
+}
 
 exports.testJujuBootstrapFails = function(test)
 {
+	test.expect(1);
+	bad_juju = new Juju('bad-env');
+	bad_juju.bootstrap(function(err)
+	{
+		test.notEqual(err, null, 'Make sure we actually get an error');
+		test.done();
+	});
 	
 }
 
-/*
-exports.testJujuNotBootstrapped = function(test)
+exports.testJujuBootstrap = function(test)
 {
-	juju.bootstrap('/home/marco', {environment: "omg"}, function(err, c, j)
+	test.expect(1);
+	juju.bootstrap(function(err)
 	{
-		test.ifError(err);
+		test.strictEqual(err, null, 'successful bootstrap');
 		test.done();
 	});
-};
-*/
+}
+
