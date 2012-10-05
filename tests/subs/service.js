6fb4271dc058bf61056a6e26af5ddcd0137bf54d
Marco Ceppi <marco@ceppi.net>
2012-10-05 16:50:14 -0400

    Service tests, deployment

--- tests/subs/service.js
+++ tests/subs/service.js
@@ -1,32 +1,96 @@
 var Juju = require('../../index.js', true);
 
-exports.setUp = function(cb)
-{
-	// Create a dummy environment
-	good_juju = new Juju('stupid');
-	bad_juju = new Juju('bad-env');
-	cb();
-}
-
-exports.tearDown = function(cb)
-{
-	good_juju, bad_juju = null;
-	cb();
-}
-
 exports.testJujuExists = function(test)
 {
 	test.ok(true, "Juju exists, we can continue");
 	test.done();
 }
 
-exports.testJujuStatusFails = function(test)
+exports.bootstrap = 
 {
-	test.expect(2);
-	bad_juju.status(function(err, data)
+	setUp: function(cb)
 	{
-		test.notStrictEqual(err, null, 'Status errored out');
-		test.strictEqual(data.bootstrapped, false, 'Wrapper header is correct');
-		test.done();
-	});
+		// Create a dummy environment
+		bad_juju = new Juju('bad-env');
+		good_juju = new Juju('stupid');
+		service_name_juju = new Juju('stupid');
+		opts_juju = new Juju('stupid');
+
+		old_exec = good_juju._exec;
+		good_juju._exec = function(cmd, cfg, ncb)
+		{
+			old_exec(cmd, cfg, function(err, results)
+			{
+				if( err.code < 2 )
+				{
+					return ncb(null, {});
+				}
+
+				ncb(err, results);
+			});
+		}
+
+		service_name_juju._exec = function(cmd, cfg, ncb)
+		{
+			if( cmd == 'juju deploy wordpress my_blog' )
+			{
+				return ncb(null, {});
+			}
+
+			ncb(new Error('Failed to deploy'), null);
+		}
+
+		opts_juju._exec = function(cmd, cfg, ncb)
+		{
+			if( cmd == 'juju deploy --constraints="instance-type=f1.fake" wordpress' )
+			{
+				return ncb(null, {});
+			}
+
+			ncb(new Error('Failed to deploy'), null);
+		}
+
+		cb();
+	},
+	tearDown: function(cb)
+	{
+		good_juju, bad_juju, service_name_juju, opts_juju = null;
+		cb();
+	},
+	testJujuDeployFails: function(test)
+	{
+		test.expect(1);
+		bad_juju.deploy('wordpress', function(err, data)
+		{
+			test.notStrictEqual(err, null, 'Deploy errors out');
+			test.done();
+		});
+	},
+	testJujuDeploy: function(test)
+	{
+		test.expect(1);
+		good_juju.deploy('wordpress', function(err, data)
+		{
+			test.strictEqual(err, null, 'Deploy runs "cleanly"');
+			test.done();
+		});
+	},
+	testJujuDeployServiceName: function(test)
+	{
+		test.expect(1);
+		service_name_juju.deploy('wordpress', 'my_blog', function(err, data)
+		{
+			test.strictEqual(err, null, 'Deploy charm with service_name');
+			test.done();
+		});
+	},
+	testJujuDeployOptions: function(test)
+	{
+		test.expect(1);
+		opts_juju.deploy('wordpress', {'constraints': 'instance-type=f1.fake'}, function(err)
+		{
+			test.strictEqual(err, null, 'Deploy charm with additional command line options');
+			test.done();
+		});
+	}
 }
